5532f26a7f796428f46cdfb2416b13d113ceb210
Ernesto Osuna <ernesto.osuna@unosquare.com>
2024-12-05 20:04:59 -0600

    postparser

---
+++ public/fe-app/postParser.js
@@ -0,0 +1,16 @@
+function postParser(obj) {
+  if (obj.id && obj.title && obj.data) {
+    return {
+      id: obj.id,
+      title: obj.title,
+      data: obj.data,
+      tags: Array.isArray(obj.tags) ? obj.tags : [],
+      // ^---- parse instead of validate,
+      // in this case we want to not get send wrong tags like undefined, numbers, or other values
+    };
+  }
+}
+
+module.exports = {
+  postParser,
+};
