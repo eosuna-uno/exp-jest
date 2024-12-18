b418b426c06949659a8f608aa41288f2781b3861
Ernesto Osuna <ernesto.osuna@unosquare.com>
2024-12-18 11:06:42 -0600

    practice default

---
+++ tests/practice.test.js
@@ -0,0 +1,8 @@
+import { describe, test, expect, it } from "@jest/globals";
+import { text } from "express";
+
+describe("test", () => {
+  test("1+1", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
